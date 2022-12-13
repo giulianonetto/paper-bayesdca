9ce9e5fc6470ed029a28c1a7d55ac6b0c5b015ed
g_netto <giuliano.netto@biome-hub.com>
2022-12-13 17:25:54 -0300

    updayte bayesdca

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM giulianocruz/rstudio:0.0.4
 
 RUN R -e "devtools::install_version('targets', version = '0.14.0', dependencies = T)"
 RUN R -e "devtools::install_version('OptimalCutpoints', version = '1.1.5', dependencies = T)"
-RUN R -e "devtools::install_github('giulianonetto/bayesdca@d6fadb8efc186ddd99e999d25479b3489948d537')"
+RUN R -e "devtools::install_github('giulianonetto/bayesdca@cf53183d22aba7b54bcd8c664b68ca83973700a2')"
--- analysis.r
+++ analysis.r
@@ -1,6 +1,8 @@
 library(tidyverse)
 theme_set(theme_bw(base_size = 14))
 library(bayesDCA)
+# prior strength
+plot(ts, pmax(50 - 300 * ts + 300 * ts^2, 5))
 
 load(
     here::here("data/gusto.rda")
@@ -24,7 +26,7 @@ test_data$phat1 <- predict(
     newdata = test_data,
     type = "response"
 )
-thresholds <- seq(0, 0.99, 0.02)
+thresholds <- seq(0, 1, 0.01)
 plot_gusto <- plot_bdca_vs_rmda(
     dataset = test_data,
     outcomes = "day30",
@@ -46,3 +48,45 @@ plot_gusto2 <- plot_bdca_vs_rmda(
 ) +
     theme(legend.position = c(.7, .7)) +
     labs(title = NULL)
+
+df <- data.frame(
+    outcomes = test_data$day30,
+    preds = test_data$phat1
+)
+
+system.time({
+    du0 <- dca(
+        df,
+        thresholds = thresholds,
+        prior_only = TRUE,
+        refresh = 1
+    )
+})
+system.time({
+    du1 <- dca(
+        df,
+        thresholds = thresholds,
+        prior_only = FALSE,
+        refresh = 1
+    )
+})
+system.time({
+    di0 <- dca(
+        df,
+        thresholds = thresholds,
+        prior_only = TRUE,
+        constant_prior = FALSE,
+        prior_sample_size = 25,
+        refresh = 1
+    )
+})
+system.time({
+    di1 <- dca(
+        df,
+        thresholds = thresholds,
+        prior_only = FALSE,
+        constant_prior = FALSE,
+        prior_sample_size = 25,
+        refresh = 1
+    )
+})
