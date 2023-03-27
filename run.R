06a382c831def11122cbf9cb80665d3940e908e7
g_netto <giuliano.netto@biome-hub.com>
2023-03-26 22:44:43 -0700

    set up sim

--- run.R
+++ run.R
@@ -5,6 +5,6 @@
 # See https://books.ropensci.org/targets/hpc.html
 # to learn about your options.
 
-targets::tar_make()
+targets::tar_make(names = c("simulation_survival_outcomes", "plot_simulation_survival_outcomes"))
 # targets::tar_make_clustermq(workers = 2) # nolint
 # targets::tar_make_future(workers = 2) # nolint
