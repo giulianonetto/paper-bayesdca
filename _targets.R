d281d4c6ddabac15a83990cc86106a9a98587858
giuliano-cruz <cruz.gnf@gmail.com>
2024-01-18 18:08:22 +0000

    new sim config

--- _targets.R
+++ _targets.R
@@ -6,7 +6,7 @@ source(here("R/pipeline_functions.r"))
 options(tidyverse.quiet = TRUE)
 
 # global seed
-.seed <- 19012024
+.seed <- 20012024
 
 # decision thresholds used in simulations and examples
 simulation_thresholds <- c(
