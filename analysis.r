c42638a13862db0879b3b53a6e41afbfcf4695f8
giuliano-cruz <cruz.gnf@gmail.com>
2024-09-24 22:57:05 +0000

    wip new setting

--- analysis.r
+++ analysis.r
@@ -46,43 +46,40 @@ simulate_data <- function(n) {
 }
 
 {
-    n <- 2e6
+    n <- 3e5
     x1 <- rnorm(n)
     x2 <- rnorm(n)
     x3 <- rnorm(n)
     x4 <- rnorm(n)
-    true_lp <- -2.5 + 0.5 * x1 + -0.5 * x2 + 1.5 * x3 + -1.7 * x4
-    lp0 <- -1.85 + .5 * x1 + -.5 * x2
-    lp1 <- -1.7 + 0.4 * x1 + 1.1 * x3
-    true_y <- rbinom(n, size = 1, prob = plogis(true_lp))
-    y0 <- rbinom(n, size = 1, prob = plogis(lp0))
-    y1 <- rbinom(n, size = 1, prob = plogis(lp1))
+    true_lp <- -2.5 + 0.5 * x1 + -1.5 * x2 + 1.5 * x3 + -1.7 * x4
+    # lp0 <- -3.5 + 0.2 * x1 - 1.5 * x2 + -1.5 * x4
+    lp0 <- -4.5 + 0.38 * x1 - 2.2 * x2 + -2.2 * x4
+    lp1 <- -1.5 + 0.3 * x1 + 0.9 * x3
+    y <- rbinom(n, size = 1, prob = plogis(true_lp))
+    d <- data.frame(outcomes = y, phat0 = plogis(lp0), phat1 = plogis(lp1), true_p = plogis(true_lp))
+    f <- bayesDCA::dca(d, thresholds = seq(0, 0.3, 0.01))
+    .calib_phat0 <- get_calibration_binary(f$.data$outcomes, f$.data$phat0)
+    .calib_phat1 <- get_calibration_binary(f$.data$outcomes, f$.data$phat1)
     data.frame(
-        type = c("true_y", "y0", "y1"),
-        prev = c(mean(true_y), mean(y0), mean(y1)),
+        model = c("phat0", "phat1"),
+        true_prevalence = rep(mean(f$.data$outcomes), 2),
+        average_prediction = c(mean(f$.data$phat0), mean(f$.data$phat1)),
+        calibration_oe = c(.calib_phat0$oe, .calib_phat1$oe),
+        calibration_slope = c(.calib_phat0$slope, .calib_phat1$slope),
         auc = c(
-            pROC::auc(pROC::roc(true_y, true_lp, quiet = TRUE)),
-            pROC::auc(pROC::roc(true_y, lp0, quiet = TRUE)),
-            pROC::auc(pROC::roc(true_y, lp1, quiet = TRUE))
+            pROC::auc(pROC::roc(f$.data$outcomes, f$.data$phat0, quiet = TRUE)),
+            pROC::auc(pROC::roc(f$.data$outcomes, f$.data$phat1, quiet = TRUE))
         )
     ) %>%
         dplyr::mutate_if(is.numeric, round, 2) %>%
         print()
-    # rms::val.prob(plogis(lp0), true_y, xlab = "plogis(lp0)")
-    # rms::val.prob(plogis(lp1), true_y, xlab = "plogis(lp1)")
-    d <- data.frame(outcomes = true_y, phat0 = plogis(lp0), phat1 = plogis(lp1), true_p = plogis(true_lp))
-    # f <- bayesDCA::dca(d, thresholds = c(0.1, 0.15, 0.2, 0.25, 0.3))
-    f <- bayesDCA::dca(d, thresholds = seq(0, 0.3, 0.01))
-    plot(f)
-    # plot(f)
-    p1 <- bayesDCA::plot_delta(f, strategies = c("true_p", "phat1"), type = "pairwise") +
-        ggplot2::coord_cartesian(ylim = c(-0.01, 0.1))
+
     p2 <- bayesDCA::plot_delta(f, strategies = c("phat1", "phat0"), type = "pairwise") +
-        ggplot2::coord_cartesian(ylim = c(-0.01, 0.1))
+        ggplot2::coord_cartesian(ylim = c(-0.04, 0.1))
     p3 <- bayesDCA::plot_delta(f, strategies = c("phat0", "phat1"), type = "useful") +
-        ggplot2::coord_cartesian(ylim = c(-0.01, 0.1)) +
+        ggplot2::coord_cartesian(ylim = c(-0.04, 0.1)) +
         ggplot2::theme(legend.position = "inside", legend.position.inside = c(0.25, 0.75))
-    ggpubr::ggarrange(plot(f) + ggplot2::theme(legend.position = "top"), p1, p2, p3, ncol = 2, nrow = 2)
+    ggpubr::ggarrange(plot(f) + ggplot2::theme(legend.position = "top"), p2, p3, ncol = 3, nrow = 1)
 }
 rms::val.prob(plogis(lp0), true_y, xlab = "plogis(lp0)")
 rms::val.prob(plogis(lp1), true_y, xlab = "plogis(lp1)")
